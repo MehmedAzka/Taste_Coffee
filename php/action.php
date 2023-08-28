b81e785b3d1ee879a5f14e3ccc41314b75e4bb75
MehmedAzka <mehmedazka28@gmail.com>
2023-08-28 21:27:36 +0700

    next step

--- php/action.php
+++ php/action.php
@@ -6,7 +6,6 @@ if (isset($_POST['id_order'])) {
     $sql = "SELECT * FROM order_item WHERE id_order=$id";
 
     $order = $conn->query($sql);
-
-    var_dump(mysqli_fetch_array($order));
+    
 }
 ?>
